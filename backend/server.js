541dc1edaf71f4ed31ae77172e4c6654e785b7f2
Ozair-Hassan <ozair.hassan123@gmail.com>
2023-12-23 16:45:18 +0500

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+import express from 'express'
+import connectDB from './config/db.js'
 import dotenv from 'dotenv'
+
 const config = dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+connectDB()
 console.log(`Running on port: ${process.env.PORT}`)
